1cf7b73f4c9d1f3356cb8d517fe54a7290ce532b
manhdinh811 <bongua1xd@gmail.com>
2023-09-19 13:27:13 +0700

    add main

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+A = np.array([(1, 2), (3, 4)])
+B = np.array([5, 6])
+A1 = np.linalg.inv(A)  # tạo ma trận nghich đảo
+print(A)
+print(B)
+print(A1)
+X = np.dot(A1, B)
+print('Nghiem cua he:', X)
+print('lam thu')
+print('aaaaaaaaaaaaaaaaaaaaaaaa')
